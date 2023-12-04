cc80a96e020615cba7fc758e3ecdd9fce524e1b5
ludha <1214002@std.ulbi.ac.id>
2023-12-04 12:42:32 +0700

    push

---
+++ js/crooots.js
@@ -0,0 +1,30 @@
+import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
+import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
+
+function PostLogIn(){
+    let target_url = "https://asia-southeast2-hppinjam.cloudfunctions.net/adminlogin";
+    let tokenkey = "token";
+    let tokenvalue = "57680f8af4ead19a4b200e10466a9f8be9abf928425835df41797523db0559c5c33338060ecbce4f4f3c607ce5053ca326b3bdb475a4522145e2ff8f92beb95f"; 
+    let datainjson = {
+        "username": getValue("username"),
+        "password": getValue("password")
+    }
+
+    postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
+
+}
+function responseData(result) {
+    
+    alert(result.message)
+    if (result.status === true) {
+        setCookieWithExpireHour("token", result.token, 2);
+        // Jika pesan adalah "Selamat Datang", arahkan ke halaman dashboard.
+        window.location.href = "dashboard.html"; // Gantilah "error.html" dengan halaman error yang sesuai.
+    } else {
+        
+        window.location.href = "hp.html";
+    }
+}
+
+document.getElementById("buttonlogin").addEventListener("click",PostLogIn)
--- js/loginadmin.js
+++ js/loginadmin.js
@@ -1,25 +1,64 @@
-// loginadmin.js
+$(document).ready(function () {
+  // Event listener untuk form login
+  $(".login-form form").on("submit", function (event) {
+    event.preventDefault(); // Menghentikan pengiriman formulir
 
-// Mock data for admin credentials
-const adminCredentials = {
-  username: 'ludha',
-  password: 'ludha'
-};
+    var email = $("input[type='text']").val(); 
+    var password = $("input[type='password']").val(); 
 
-// Function to handle the login process
-function loginAdmin(username, password) {
-  // Check if the provided credentials match the admin credentials
-  if (username === adminCredentials.username && password === adminCredentials.password) {
-    console.log('Login successful. Welcome, admin!');
-    // Perform additional actions after successful login if needed
-  } else {
-    console.log('Login failed. Please check your username and password.');
-  }
-}
+    // Gantilah kondisi ini dengan logika validasi yang sesuai
+    if (email === "contoh@email.com" && password === "riziq") {
+      // Login berhasil
+      Swal.fire({
+        icon: 'success',
+        title: 'Login Berhasil',
+        text: 'Anda telah berhasil login',
+        showConfirmButton: false,
+        timer: 1500
+      });
 
-// Example usage
-const usernameInput = 'ludha';
-const passwordInput = 'ludha';
+      // Redirect ke halaman dashboard
+      window.location.href = 'hp.html';
+    } else {
+      // Login gagal
+      Swal.fire({
+        icon: 'error',
+        title: 'Login Gagal',
+        text: 'Email atau password salah',
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }
+  });
 
-// Call the login function with the provided credentials
-loginAdmin(usernameInput, passwordInput);
+  // Event listener untuk form signup
+  $(".signup-form form").on("submit", function (event) {
+    event.preventDefault(); // Menghentikan pengiriman formulir
+
+    var name = $(".signup-form input[name='name']").val();
+    var email = $(".signup-form input[name='email']").val();
+    var password = $(".signup-form input[name='password']").val();
+
+    // Gantilah kondisi ini dengan logika validasi yang sesuai
+    if (name && email && password) {
+      // Sign-up berhasil
+      Swal.fire({
+        icon: 'success',
+        title: 'Sign-up Berhasil',
+        text: 'Anda telah berhasil mendaftar',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      // Anda dapat melakukan tindakan lain yang sesuai dengan sign-up yang berhasil
+    } else {
+      // Sign-up gagal
+      Swal.fire({
+        icon: 'error',
+        title: 'Sign-up Gagal',
+        text: 'Mohon isi semua kolom dengan benar',
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }
+  });
+});
