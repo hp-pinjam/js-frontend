1e4df4046b10c0fac6ef140502188bfafb3315cf
ludha <1214002@std.ulbi.ac.id>
2023-12-04 13:57:14 +0700

    push

--- js/crooots.js
+++ js/crooots.js
@@ -27,4 +27,4 @@ function responseData(result) {
     }
 }
 
-document.getElementById("buttonlogin").addEventListener("click",PostLogInAdmin)
+document.getElementById("adminbuttonlogin").addEventListener("click",PostLogInAdmin)
