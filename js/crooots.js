b723dc5c64c5d711d704e45ecff6b02e0224ae8a
ludha <1214002@std.ulbi.ac.id>
2023-12-04 14:05:12 +0700

    push

--- js/crooots.js
+++ js/crooots.js
@@ -2,7 +2,7 @@ import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
 
-function PostLogInAdmin(){
+function PostLogIn(){
     let target_url = "https://asia-southeast2-hppinjam.cloudfunctions.net/adminlogin";
     let tokenkey = "token";
     let tokenvalue = "57680f8af4ead19a4b200e10466a9f8be9abf928425835df41797523db0559c5c33338060ecbce4f4f3c607ce5053ca326b3bdb475a4522145e2ff8f92beb95f"; 
@@ -27,4 +27,4 @@ function responseData(result) {
     }
 }
 
-document.getElementById("adminbuttonlogin").addEventListener("click",PostLogInAdmin)
+document.getElementById("adminbuttonlogin").addEventListener("click",PostLogIn)
